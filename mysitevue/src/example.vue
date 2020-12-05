424362b2e0098d05ad43c0fd9e6dad8da7dfd2cc
AlexBirdSong <alexwhitechoco@gmail.com>
2020-12-05 16:54:47 +0300

    components & .vue files

---
+++ mysitevue/src/example.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Пробный текст</h1>
+        <app-input></app-input>
+    </div>
+</template>
+
+<script>
+
+    import Input from './field.vue'
+
+    export default {
+        components: {
+            'app-input': Input
+        }
+}
+</script>
